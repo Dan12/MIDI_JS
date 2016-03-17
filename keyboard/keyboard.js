175998c854293cbbd2b551c112b041539c999ed8
Dan W <dantech88@yahoo.com>
2016-03-17 02:28:36 +0000

    compiled and started splitting keyboard

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -297,6 +297,7 @@ var Keyboard_Space = new function(){
         $(".sound_pack_button").css("background-color","white");
         $(".sound_pack_button_"+(currentSoundPack+1)).css("background-color","rgb(255,160,0)");
         $(".soundPack").html("Sound Pack: "+(currentSoundPack+1));
+        // set pressures for buttons in new sound pack
         for(var i = 0; i < 4; i++){
             for(var j = 0; j < 12; j++){
                 var press = false;
@@ -391,23 +392,23 @@ var Keyboard_Space = new function(){
     // TODO: convert keypairs to dictionarys/objects
     // {49:0,50:1,51:2...}
     
-    // ascii key mappings to array index
-    var keyPairs = [49,50,51,52,53,54,55,56,57,48,189,187,
-                    81,87,69,82,84,89,85,73,79,80,219,221,
-                    65,83,68,70,71,72,74,75,76,186,222,13,
-                    90,88,67,86,66,78,77,188,190,191,16,-1];
+    // // ascii key mappings to array index
+    // var keyPairs = [49,50,51,52,53,54,55,56,57,48,189,187,
+    //                 81,87,69,82,84,89,85,73,79,80,219,221,
+    //                 65,83,68,70,71,72,74,75,76,186,222,13,
+    //                 90,88,67,86,66,78,77,188,190,191,16,-1];
                 
-    // alternate keys for firefox
-    var backupPairs = [49,50,51,52,53,54,55,56,57,48,173,61,
-                       81,87,69,82,84,89,85,73,79,80,219,221,
-                       65,83,68,70,71,72,74,75,76,59,222,13,
-                       90,88,67,86,66,78,77,188,190,191,16,-1];
+    // // alternate keys for firefox
+    // var backupPairs = [49,50,51,52,53,54,55,56,57,48,173,61,
+    //                   81,87,69,82,84,89,85,73,79,80,219,221,
+    //                   65,83,68,70,71,72,74,75,76,59,222,13,
+    //                   90,88,67,86,66,78,77,188,190,191,16,-1];
                    
-    // letter to show in each button
-    var letterPairs = ["1","2","3","4","5","6","7","8","9","0","-","=",
-                       "Q","W","E","R","T","Y","U","I","O","P","[","]",
-                       "A","S","D","F","G","H","J","K","L",";","'","\\n",
-                       "Z","X","C","V","B","N","M",",",".","/","\\s","NA"];
+    // // letter to show in each button
+    // var letterPairs = ["1","2","3","4","5","6","7","8","9","0","-","=",
+    //                   "Q","W","E","R","T","Y","U","I","O","P","[","]",
+    //                   "A","S","D","F","G","H","J","K","L",";","'","\\n",
+    //                   "Z","X","C","V","B","N","M",",",".","/","\\s","NA"];
     
     // current soundpack (0-3)
     var currentSoundPack = 0;
