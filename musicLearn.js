c1251bf34fbd97f5f958c7798d37dfcac03c7489
Dan W <dantech88@yahoo.com>
2016-03-17 03:23:01 +0000

    init music learn

---
+++ musicLearn.js
@@ -0,0 +1,12 @@
+var MusicLearnSpace = new function(){
+
+    // sample backend for keyboard
+    this.init = function(){
+        return new MusicLearn();
+    }
+    
+    var MusicLearn = function(){
+        console.log("New Music Learn Object");
+    }
+    
+}
